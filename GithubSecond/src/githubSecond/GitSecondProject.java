2fdc1cb10da61b6777fb946f44d390c6632b0ccb
AnatolieClaichnet <anatolieclaichnet@gmail.com>
2020-01-27 22:13:48 +0200

    Second project

---
+++ GithubSecond/src/githubSecond/GitSecondProject.java
@@ -0,0 +1,10 @@
+package githubSecond;
+
+public class GitSecondProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Hello to git");
+	}
+
+}
